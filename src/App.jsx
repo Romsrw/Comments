40920017bb6d1b4e563d7f21e51742686e210b08
Roma <rmahsurw@gmail.com>
2021-12-08 02:05:28 +0500

    install pakages mui, jsdoc, create app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Container } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyle = makeStyles({
+  app: {
+    margin: "20px 0",
+    height: "calc(100vh - 40px)",
+    boxShadow: "0px 5px 10px 2px rgba(34, 60, 80, 0.2)",
+  },
+});
+
 const App = () => {
-  return <div>comments</div>;
+  const classes = useStyle();
+  return <Container className={classes.app}>comments</Container>;
 };
 
 export default App;
